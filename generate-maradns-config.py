8710cdbf1280b8439b5e886c36b71bb5ee9ccf77
Guenther Starnberger <gst@sysfrog.org>
2011-05-29 15:16:05 +0200

    fixed json query arguments (only 500 results with default arguments)

--- generate-maradns-config.py
+++ generate-maradns-config.py
@@ -11,7 +11,7 @@ import string
 import sys
 
 def get_json(server, user, passwd):
-    params = '{"version": "1.1","method": "name_scan","params": [],"id": 1}'
+    params = '{"version": "1.1","method": "name_scan","params": ["","21474836487"],"id": 1}'
     auth = 'Basic ' + string.strip(base64.encodestring(user + ':' + passwd))
     headers = {"Content-type": "application/json", "Authorization": auth}
     conn = httplib.HTTPConnection(server)
